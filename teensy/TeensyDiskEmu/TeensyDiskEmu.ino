7182681752a4183eb0b51bd5efb57842030492c3
calphool <jrounceville@hotmail.com>
2019-01-04 16:04:15 -0600

    current work v3.0 code

--- teensy/TeensyDiskEmu/TeensyDiskEmu.ino
+++ teensy/TeensyDiskEmu/TeensyDiskEmu.ino
@@ -1,3 +1,17 @@
+//for use with TRSHDD board v3.0
+
+#include <Metro.h>
+
+#include <BlockDriver.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <sdios.h>
+#include <SysCall.h>
+#include <StringStream.h>
+
+
 
 #include <stdarg.h>
 
@@ -42,6 +56,10 @@
 #define D0 2
 #define NOTHING 0
 
+#define IN 0
+#define OUT 1
+
+
 volatile int activeInterrupt = NOTHING;
 volatile int a0;
 volatile int a1;
@@ -49,13 +67,75 @@ volatile int port_a;
 volatile int port_c;
 volatile int port_d;
 
-#define IN 0
-#define OUT 1
+volatile int currentDrive = 0xff;
+volatile int trackNum = 0;
+volatile int sectorNum = 0;
+volatile byte dataRegister = 0;
+volatile byte commandRegister = 0;
+volatile int byteCtr;
+
+volatile byte interruptStatus = 0x80;
+
+typedef unsigned int boolean_t;
 #define FALSE 0
-#define TRUE 1
-volatile int iDirection = 2;
+#define TRUE !FALSE
+typedef union {
+        struct {
+                boolean_t Type1_Busy:1;
+                boolean_t Type1_IndexHole:1;
+                boolean_t Type1_HeadAtZero:1;
+                boolean_t Type1_CRCError:1;
+                boolean_t Type1_TrackNotVerified:1;
+                boolean_t Type1_HeadEngaged:1;
+                boolean_t Type1_WriteProtected:1;
+                boolean_t Type1_MotorsOff:1;   
+        };
+        struct {
+                boolean_t Type2_Busy:1;
+                boolean_t Type2_DataRequest:1;
+                boolean_t Type2_LostData:1;
+                boolean_t Type2_CRCError:1;
+                boolean_t Type2_RecordNotFound:1;
+                boolean_t Type2_RecTypeOrFault1:1;
+                boolean_t Type2_RecTypeOrFault2:1;
+                boolean_t Type2_MotorsOff:1;
+        };
+        struct {
+                boolean_t Type3_Busy:1;
+                boolean_t Type3_DataRequest:1;
+                boolean_t Type3_LostData:1;
+                boolean_t Type3_Unused1:1;
+                boolean_t Type3_Unused2:1;
+                boolean_t Type3_RecTypeOrFault1:1;
+                boolean_t Type3_RecTypeOrFault2:1;
+                boolean_t Type3_MotorsOff:1;                      
+        };
+        byte rawValue;
+} statusRegisterDef;
+
+volatile statusRegisterDef statusRegister;
+
+volatile int iIndexHole = 1;
+volatile int iTrackDirection = IN;
+
+
+volatile int iBusDirection = 2;
 volatile byte b = 0;
 volatile int busvalue;
+volatile byte currentCommand = 0;
+#define CMD_NONE 0
+#define CMD_READADDRESS 1
+
+String strmOutput;
+String disk1FileName = "NEWDOS_80sssd_jv1.DSK";
+
+
+SdFatSdioEX sdEx;
+File file;
+
+File disk1File;
+
+File diskImage1;
 
 
 
@@ -74,9 +154,11 @@ void p(char *fmt, ... ){
 
 /* direct data bus pins outward */
 inline void dataOutMode() {
-  if(iDirection == OUT)
-    return;
-  iDirection = OUT;
+  if(iBusDirection == OUT)
+     return;
+  iBusDirection = OUT;
+
+/*
   pinMode(D7,OUTPUT);
   pinMode(D6,OUTPUT);
   pinMode(D5,OUTPUT);
@@ -85,14 +167,20 @@ inline void dataOutMode() {
   pinMode(D2,OUTPUT);
   pinMode(D1,OUTPUT);
   pinMode(D0,OUTPUT);
+*/
+
+  GPIOA_PDDR = 143392;
+  GPIOC_PDDR = 2318;
+  GPIOD_PDDR = 253;
 }
 
 
 /* direct data bus pins inward */
 inline void dataInMode() {
-  if(iDirection == IN)
-    return;
-  iDirection = IN;
+  if(iBusDirection == IN)
+      return;
+  iBusDirection = IN;
+  /*
   pinMode(D7,INPUT);
   pinMode(D6,INPUT);
   pinMode(D5,INPUT);
@@ -101,6 +189,10 @@ inline void dataInMode() {
   pinMode(D2,INPUT);
   pinMode(D1,INPUT);
   pinMode(D0,INPUT);
+  */
+  GPIOA_PDDR = 131104;
+  GPIOC_PDDR = 2310;
+  GPIOD_PDDR = 96;    
 }
 
 
@@ -114,8 +206,8 @@ void pinSetup() {
   pinMode(23,OUTPUT);                 // L1 LEDs
   pinMode(39,OUTPUT);                 // nothing, unused pin
   pinMode(38,OUTPUT);                 // nothing, unused pin
-  pinMode(A1, INPUT_PULLUP);          // A1 from TRS-80
-  pinMode(A0, INPUT_PULLUP);          // A0 from TRS-80
+  pinMode(A1, INPUT);          // A1 from TRS-80
+  pinMode(A0, INPUT);          // A0 from TRS-80
   pinMode(INTERUPT_TO_TRS80, OUTPUT); // pin to trigger interrupts on the TRS-80
   pinMode(_37ECWR, INPUT);            // pin to detect when write to address 37EC has been triggered (low)
   pinMode(_37E8WR, INPUT);            // pin to detect when write to address 37E8 has been triggered (low)
@@ -128,7 +220,16 @@ void pinSetup() {
   pinMode(_37E8RD,INPUT);             // pin to detect when read from address 37E8 has been triggered (low)
   pinMode(_37E4RD,INPUT);             // pin to detect when read from address ﻿37E4 has been triggered (low)
   pinMode(_37E0RD,INPUT);             // pin to detect when read from address 37E0 has been triggered (low)
-  dataInMode();
+
+  pinMode(D7,INPUT);
+  pinMode(D6,INPUT);
+  pinMode(D5,INPUT);
+  pinMode(D4,INPUT);
+  pinMode(D3,INPUT);
+  pinMode(D2,INPUT);
+  pinMode(D1,INPUT);
+  pinMode(D0,INPUT);
+  iBusDirection = IN;
 }
 
 
@@ -162,12 +263,14 @@ void initialPinState() {
 /* reset the latch that's tied to TRS-80 WAIT* line */
 inline void resetWaitLatch() {
   //digitalWrite(FF_PRE,HIGH);
-  digitalWrite(FF_CLR,LOW);
-  //delayMicroseconds(1);
-  digitalWrite(FF_CLR,HIGH);
+  digitalWriteFast(FF_CLR,LOW);
+  delayMicroseconds(1);
+  digitalWriteFast(FF_CLR,HIGH);
+  delayMicroseconds(1);
+  digitalWriteFast(INTERUPT_TO_TRS80, HIGH);
 }
 
-/*
+
 void BinaryStrZeroPad(int Number,char ZeroPadding){
 signed char i=ZeroPadding;
 
@@ -179,7 +282,7 @@ signed char i=ZeroPadding;
 
         Serial.println();
 }
-*/
+
 
 inline int convertBusValue() {
   dataInMode();
@@ -187,86 +290,329 @@ inline int convertBusValue() {
   port_c = GPIOC_PDIR;
   port_d = GPIOD_PDIR;
   
-  unsigned int bv = (port_d & 0x0001);
-  bv = bv + ((port_a & 0x1000) >> 11);   
-  bv = bv + ((port_a & 0x2000) >> 11);   
-  bv = bv + ((port_d & 0x0080) >> 4);    
-  bv = bv + ((port_d & 0x0010));         
-  bv = bv + ((port_d & 0x0004) << 3);    
-  bv = bv + ((port_d & 0x0008) << 3);    
-  bv = bv + ((port_c & 0x0008) << 4);    
+  unsigned int bv = (port_d & 0x0001); //D0, pin 2
+  bv = bv + ((port_a & 0x1000) >> 11); //D1, pin 3
+  bv = bv + ((port_a & 0x2000) >> 11); //D2, pin 4
+  bv = bv + ((port_d & 0x0080) >> 4);  //D3, pin 5
+  bv = bv + ((port_d & 0x0010));       //D4, pin 6
+  bv = bv + ((port_d & 0x0004) << 3);  //D5, pin 7
+  bv = bv + ((port_d & 0x0008) << 3);  //D6, pin 8
+  bv = bv + ((port_c & 0x0008) << 4);  //D7, pin 9
   
   return bv;
 }
 
+void displayAddress(unsigned int address) {
+  if(address == 0x37e0) 
+    p("- Interrupt latch");
+  else
+  if(address == 0x37e1)
+    p("- Disk drive select latch (1)");
+  else
+  if(address == 0x37e4)
+    p("- Cassette select latch");
+  else
+  if(address == 0x37e8)
+    p("- Line printer");
+  else
+  if(address == 0x37ec) 
+    p("- disk controller command/status register");
+  else
+  if(address == 0x37ed) 
+    p("- disk track position register");
+  else
+  if(address == 0x37ee)
+    p("- disk sector register");
+  else
+  if(address == 0x37ef)
+    p("- disk data register");
+  else
+    p("- unknown");  
+}
+
+
+void setStatusRegisterToNormalType1Response() {
+    statusRegister.Type1_Busy = 0;
+    statusRegister.Type1_IndexHole = (iIndexHole == 1);
+    if(trackNum == 0) statusRegister.Type1_HeadAtZero = 1; else statusRegister.Type1_HeadAtZero = 0;
+    statusRegister.Type1_CRCError = 0;
+    statusRegister.Type1_TrackNotVerified = 0;
+    statusRegister.Type1_HeadEngaged = 1;
+    statusRegister.Type1_WriteProtected = 0;
+    statusRegister.Type1_MotorsOff = 1;
+}
+
+void invokeCommand() {
+  //
+  // Command   Type    76543210
+  //
+  // RESTORE     1     0000XXXX
+  // SEEK        1     0001XXXX
+  // STEP        1     001uXXXX    u = update track register
+  // STEP IN     1     010uXXXX
+  // STEP OUT    1     011uXXXX
+  // READ CMD    2     100mbXXX    m = muliple record, b = 128-1024 block length (0) or 16-4096 block length(1)
+  // WRITE CMD   2     101mbXXX
+  // READ ADDR   3     11000100
+  // READ TRK    3     1110010X
+  // WRITE TRK   3     11110100
+  // FORCE INTRP 4     1101XXXX
+  
+  if((commandRegister & 0xf0) == 0x00) {              // restore command
+    p(" RESTORE CMD ");
+    trackNum = 0;
+    disk1File.seek(0);
+    currentCommand = CMD_NONE;
+    setStatusRegisterToNormalType1Response();
+    interruptStatus = 0x40;
+    digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xf0) == 0x10) {         // seek command
+    p(" SEEK CMD ");
+    trackNum = dataRegister;
+    disk1File.seek(trackNum * 2560);
+    currentCommand = CMD_NONE;
+    setStatusRegisterToNormalType1Response();
+    interruptStatus = 0x40;
+    digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xe0) == 0x20) {         // step command
+    p(" STEP CMD ");
+    if(commandRegister & 0x10 == 0x10) {            // only do this stuff if the update flag is set on the track register, otherwise just generate an interupt like we complied
+      if(iTrackDirection == OUT) {
+          if(trackNum > 0)
+              trackNum--;
+      }
+      else {
+        trackNum++;
+      }
+    }
+    disk1File.seek(trackNum*2560);
+    currentCommand = CMD_NONE;
+    setStatusRegisterToNormalType1Response();
+    interruptStatus = 0x40;
+    digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xe0) == 0x40) {        // step in command
+    p(" STEP IN CMD ");
+    if(commandRegister & 0x10 == 0x10) {           // only do this stuff if the update flag is set on the track register, otherwise just generate an interupt like we complied
+      trackNum++;
+    }
+    iTrackDirection = IN; // IN is away from 0
+    currentCommand = CMD_NONE;
+    disk1File.seek(trackNum*2560);
+    setStatusRegisterToNormalType1Response();
+    interruptStatus = 0x40;
+    digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xe0) == 0x60) {        // step out command
+    p(" STEP OUT CMD ");
+    if(commandRegister & 0x10 == 0x10) {           // only do this stuff if the update flag is set on the track register, otherwise just generate an interupt like we complied
+      if(trackNum > 0) 
+         trackNum--;
+    }
+    iTrackDirection = OUT;                         // OUT is toward 0
+    currentCommand = CMD_NONE;
+    disk1File.seek(trackNum*2560);
+    setStatusRegisterToNormalType1Response();
+    interruptStatus = 0x40;
+    digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xe0) == 0x80) {        // read command
+     byteCtr = 256;
+
+     p(" READ SECTOR CMD");
+
+     statusRegister.Type2_Busy = 1;        
+     statusRegister.Type2_DataRequest = 1;
+     statusRegister.Type2_LostData = 0;
+     statusRegister.Type2_CRCError = 0;
+     statusRegister.Type2_RecordNotFound = 0;
+     statusRegister.Type2_RecTypeOrFault1 = 0;
+     statusRegister.Type2_RecTypeOrFault2 = 0;
+     statusRegister.Type2_MotorsOff = 0;
+     setDataBus(0x40);
+     digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+  }
+  else if((commandRegister & 0xe0) == 0x90) {        // write command
+     p(" WRITE CMD (unimpl) ");    
+  }
+  else if(commandRegister == 0xc4) {               // read address command
+     p(" READ ADDR (unimpl) ");
+  }
+  else if((commandRegister & 0xfe) == 0xe4) {        // read track command
+     p(" READ TRK (unimpl) ");        
+  }
+  else if(commandRegister == 0xf4) {               // write track command
+     p(" WRITE TRK (unimpl) ");        
+  }
+  else if((commandRegister & 0xf0) == 0xd0) {        // force interrupt command
+     p(" FORCE INTERRUPT ");
+     setStatusRegisterToNormalType1Response();
+     currentCommand = CMD_NONE;            
+  }
+  else {
+    p("- Unknown command: %x\n", commandRegister);
+  }
+}
+
+void statusOut() {
+  p("\nCD: %1d  TRK: %3d  SEC: %3d  DAT: %2X  SR: %2X\n\n", currentDrive, trackNum, sectorNum, dataRegister, statusRegister.rawValue);
+}
+
+void PokeFromTRS80(unsigned int address, int byt) {
+  p("\nPoke: %8X, receiving: %x ",address,byt);
+  iIndexHole = -iIndexHole;
+  
+  displayAddress(address);
+  if((address & 0xfffc) == 0x37e0) { // drive select
+    if(byt == 1)
+      currentDrive = 0;
+    else if(byt == 2)
+      currentDrive = 1;
+    else if(byt == 4)
+      currentDrive = 2;
+    else if(byt == 8)
+      currentDrive = 3;
+    else {
+      p(" ??Received nonsense value for drive number, assuming 0??\n");
+      currentDrive = 0;
+    }
+
+    setStatusRegisterToNormalType1Response(); 
+    return;
+  } 
+  else
+  if(address == 0x37ed) {
+    trackNum = byt;  // track
+    disk1File.seek(trackNum * 2560);
+    setStatusRegisterToNormalType1Response(); 
+    return;
+  }
+  else
+  if(address == 0x37ee) {
+    sectorNum = byt; // sector
+    disk1File.seek(trackNum * 2560 + sectorNum * 256);
+    setStatusRegisterToNormalType1Response(); 
+    return;
+  }
+  else
+  if(address == 0x37ef) {
+    dataRegister = byt;  // data byte
+    setStatusRegisterToNormalType1Response(); 
+    return;
+  }
+  if(address == 0x37ec) {
+    commandRegister = byt;
+    invokeCommand();
+    statusOut();
+    return;
+  }
+
+  p("Unhandled POKE!\n\n");
+}
+
+byte PeekFromTRS80(unsigned int address) {
+  p("\nPeek: %8X ",address); 
+  iIndexHole = -iIndexHole;
+  
+  displayAddress(address);
+  statusRegister.Type1_IndexHole = (iIndexHole == 1);
+  if(trackNum == 0) statusRegister.Type1_HeadAtZero = 1; else statusRegister.Type1_HeadAtZero = 0;
+
+  if(address == 0x37ec) {
+    statusOut();
+    return statusRegister.rawValue;
+    //return 0;
+  }
+
+  if(address == 0x37ef) {
+    if(statusRegister.Type2_Busy == 1) {
+     byteCtr--;
+     dataRegister = disk1File.read();
+     p(" PEEK DATA %d %x", byteCtr, dataRegister);
+
+     if(byteCtr > 0)
+        statusRegister.Type2_Busy = 1;
+     else {
+        statusRegister.Type2_Busy = 0;
+        statusRegister.Type2_MotorsOff = 1;
+     }
+    }
+    return dataRegister;
+  }
+
+  if(address == 0x37ee) {
+    return sectorNum;
+  }
+
+  if(address == 0x37ed) {
+    return trackNum;
+  }
+
+  if(address == 0x37e0) {
+    return interruptStatus;
+  }
+
+  p(" returning 0xfe - unknown behavior\n\n");
+
+  return 0xfe;
+}
+
+
 #define GET_LOW_ADDRESSES()   a0 = digitalReadFast(A0); a1 = digitalReadFast(A1);
 void _37E0WRInterrupt() {
-  GET_LOW_ADDRESSES()
   busvalue = convertBusValue();
-  activeInterrupt = _37E0WR;    
+  GET_LOW_ADDRESSES()
+  PokeFromTRS80(0x37e0 + (a1<<1) + a0, busvalue); 
   resetWaitLatch();    
-  p("37e0 wr %d %d %d\n",busvalue,a1,a0);
 }
 
 void _37E8WRInterrupt() {
-  GET_LOW_ADDRESSES()
   busvalue = convertBusValue();
-  activeInterrupt = _37E8WR;  
+  GET_LOW_ADDRESSES()
+  PokeFromTRS80(0x37e8 + (a1<<1) + a0, busvalue); 
   resetWaitLatch();
-  p("378e wr %d\n",busvalue);
-}
+} 
 
 void _37E4WRInterrupt() {
-  GET_LOW_ADDRESSES()
   busvalue = convertBusValue();
-  activeInterrupt = _37E4WR;  
+  GET_LOW_ADDRESSES() 
+  PokeFromTRS80(0x37e4 + (a1<<1) + a0, busvalue); 
   resetWaitLatch();
-  p("37e4 wr %d\n",busvalue);
 }
 
 void _37ECWRInterrupt() {
-  GET_LOW_ADDRESSES()
   busvalue = convertBusValue();
-  activeInterrupt = _37ECWR;
+  GET_LOW_ADDRESSES()
+  PokeFromTRS80(0x37ec + (a1<<1) + a0, busvalue); 
   resetWaitLatch();
-  p("37ec wr %d\n",busvalue);
 }
 
 
 void _37ECRDInterrupt() {
   GET_LOW_ADDRESSES()
-  activeInterrupt = _37ECRD;
-  b++;
-  setDataBus(b);
+  setDataBus(PeekFromTRS80(0x37ec + (a1<<1) + a0));
   resetWaitLatch();
-  p("37ec rd %d\n",b);
 }
 
 void _37E8RDInterrupt() {
   GET_LOW_ADDRESSES()
-  activeInterrupt = _37E8RD;
-  b++;
-  setDataBus(b);
+  setDataBus(PeekFromTRS80(0x37e8 + (a1<<1) + a0));
   resetWaitLatch();
-  p("37e8 rd %d\n",b);
 }
 
 void _37E4RDInterrupt() {
   GET_LOW_ADDRESSES()
-  activeInterrupt = _37E4RD;
-  b++;
-  setDataBus(b);
+  setDataBus(PeekFromTRS80(0x37e4 + (a1<<1) + a0));
   resetWaitLatch();
-  p("37e4 rd %d\n",b);
 }
 
 void _37E0RDInterrupt() {
   GET_LOW_ADDRESSES()
-  activeInterrupt = _37E0RD;
-  b++;
-  setDataBus(b);
+  setDataBus(PeekFromTRS80(0x37e0 + (a1<<1) + a0));
+  interruptStatus = 0x00;  // reset interrupt status after this is invoked
   resetWaitLatch();  
-  p("37e0 rd %d\n",b);
 }
 
 
@@ -302,6 +648,8 @@ void configureInterrupts() {
 void setup() {
   int i = 0;
   cli();
+  statusRegister.rawValue = 6;
+  
   Serial.begin(2000000); // high speed serial over USB
   pinSetup();            // set pin modes
   initialPinState();     // put pins in initial configuration
@@ -321,11 +669,64 @@ void setup() {
 
   p("F_CPU: %d\n", F_CPU);
   p("F_BUS: %d\n", F_BUS);
-  p("Ready.\n");
+
+  convertBusValue();
+  
+  StringStream stream(strmOutput);
+  
+  if (!sdEx.begin()) {
+      sdEx.initErrorHalt("SdFatSdioEX begin() failed");
+  }
+  sdEx.chvol();
+
+  sdEx.vwd()->rewind();
+  while (file.openNext(sdEx.vwd(), O_RDONLY)) {
+      if(!file.isHidden()) {
+          file.printName(&stream);
+          if (file.isDir()) {
+           stream.write('/');
+          }
+          stream.flush();
+          if(strmOutput == disk1FileName) {
+             p("Opening disk 1 file: ");
+             p((char*)strmOutput.c_str());
+             disk1File = sdEx.open(strmOutput.c_str(), FILE_READ);
+             if(!disk1File) {
+                p("\n*** ERROR:  Unable to open file ***\n");
+                L1_RED();
+                L2_RED();
+             }
+             else {
+              p("\n%d bytes in file.\n",disk1File.available());
+             }
+          }
+          strmOutput = "";
+      }
+      file.close();
+  }
+  p("\nReady.\n");
+  setStatusRegisterToNormalType1Response();
   sei();
+  //disk1File.close();
 }
 
 
-// Do nothing at this point
+Metro trs80ClockPulse = Metro(25);
+
 void loop() {
+  /*
+    if(trs80ClockPulse.check() == 1) { // invoke clock interrupt every 25ms
+       cli();
+       interruptStatus = 0x80;
+       if(iBusDirection == IN)
+          pinMode(D7,OUTPUT);
+       digitalWriteFast(D7,HIGH);
+       digitalWriteFast(INTERUPT_TO_TRS80, LOW);
+       if(iBusDirection == IN) {
+          delayMicroseconds(1);
+          pinMode(D7,INPUT);
+       }
+       sei();
+    }
+    */
 }
